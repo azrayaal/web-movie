a1f9dc112420a06a08a0963ba34cbdc2de081ee3
AzraYazid <azrayazidalkautsar@gmail.com>
2023-01-11 02:32:15 +0700

    update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -40,6 +40,21 @@ export default function Navbarr() {
                   </div>
                 </div>
               </div>
+              <div className="ml-auto mr-8">
+                <div class="flex justify-center">
+                  <div class=" xl:w-96">
+                    <div class="input-group relative flex flex-wrap items-stretch w-full ">
+                      <input
+                        type="search"
+                        class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-200 bg-gray-500 bg-clip-padding border border-solid border-gray-500 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        placeholder="Search"
+                        aria-label="Search"
+                        aria-describedby="button-addon2"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
