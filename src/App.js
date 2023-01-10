19a9f9f989bbbb4cf02eca57ed16c8ac11b440e8
AzraYazid <azrayazidalkautsar@gmail.com>
2023-01-10 15:32:10 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbarr from './components/navbar/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbarr />
+      <div className="App-body">
+        <div className="header ">
+          <h1 className="">Explore</h1>
+          <div className="text-sm text-gray-500">What are you gonna watch today?</div>
+        </div>
+      </div>
+    </>
   );
 }
 
